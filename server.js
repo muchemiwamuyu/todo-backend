045160ab2fbf1e479e1f467f43d3d2b76e83b5e9
muchemi wamuyu <muchemiedwin68@gmail.com>
2025-08-04 17:26:32 +0300

    backend and database connection updated

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
-import connectDb from "./config/db.js";
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/db.js';
+
+dotenv.config();
+const app = express()
+
+
+// middleware
+app.use(express.json());
+
+// port
+const port = process.env.PORT || 3000
 
 const startServer = async () => {
-    await connectDb();
+    try {
+        const database = await connectDb(); // await db connection
+
+        // app routes
+        app.get('/', async (req, res) => {
+            try {
+                const [rows] = await database.query('SELECT * FROM tasks;')
+                res.json({rows})
+            } catch (error) {
+                console.error('failed to connect to the database', error)
+                res.status(500).json({message: 'Failed to fetch tasks'})
+            }
+        });
+
+        app.listen(port, () => console.log(`server running on port ${port}`));
+    } catch (error) {
+        console.error('error connecting to the database', error);
+        process.exit(1)
+    }
 }
 
-startServer();
+startServer()
+
