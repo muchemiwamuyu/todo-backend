f76544f57525cc4639e8df41f0bb76ea102f4248
muchemi wamuyu <muchemiedwin68@gmail.com>
2025-08-06 23:21:43 +0300

    updated database configuration

--- config/db.js
+++ config/db.js
@@ -6,10 +6,10 @@ dotenv.config();
 const connectDb = async () => {
     try {
         const database = await mysql.createConnection({
-            host: process.env.host,
-            user: process.env.user,
-            password: process.env.password,
-            database: process.env.database
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
         })
         console.log('Mysql connected');
         return database;
