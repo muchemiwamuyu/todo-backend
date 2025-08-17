de6528aee140ede31ff432fec1979d70768ef358
muchemi wamuyu <muchemiedwin68@gmail.com>
2025-08-17 21:01:54 +0300

    adding tasks routes created

--- config/db.js
+++ config/db.js
@@ -1,22 +1,22 @@
-import mysql from 'mysql2/promise'
-import dotenv from 'dotenv'
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectDb = async () => {
+async function connectDb() {
     try {
-        const database = await mysql.createConnection({
+        const connection = await mysql.createConnection({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE
-        })
+        });
         console.log('Mysql connected');
-        return database;
+        return connection;
     } catch (error) {
-        console.error('error connecting to mysql', error)
-        process.exit(1)
+        console.error('error connecting to mysql', error);
+        process.exit(1);
     }
-}
+};
 
 export default connectDb;
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,8 @@
 import express from 'express'
-import { createTasks, updateTasks } from '../controllers/taskController.js'
+import { createTasks } from '../controllers/taskController.js'
 
 const router = express.Router()
 
 router.post('/tasks', createTasks);
-router.put('/task/:id', updateTasks)
 
 export default router
